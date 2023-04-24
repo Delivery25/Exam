667c9e32421a69ae1b640a4748d36172abbec763
Di <dds.03@mail.ru>
2023-04-24 09:58:48 +0300

    Добавьте файлы проекта.

---
+++ Exam/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Exam.Pages;
+using System;
+using System.Windows;
+
+namespace Exam
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Manager.MainFrame = MainFrame;
+            MainFrame.Navigate(new LoginPage());
+        }
+
+        /// <summary>
+        /// Событие при смене содержимого в MainFrame
+        /// </summary>
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            BackButton.Visibility = MainFrame.CanGoBack ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        /// <summary>
+        /// Событие по нажатию на кнопку возврата на предыдущую страницу
+        /// </summary>
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.GoBack();
+        }
+    }
+}
---
+++ Exam/Pages/ItemsTablePage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Exam.Pages
+{
+    /// <summary>
+    /// Interaction logic for ItemsTablePage.xaml
+    /// </summary>
+    public partial class ItemsTablePage : Page
+    {
+        public ItemsTablePage()
+        {
+            InitializeComponent();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
